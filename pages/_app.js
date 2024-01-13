d62f97637b6adca836db61783109d22ed5d70a00
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-13 13:21:31 -0500

    [feature] Importing router and layoutcomponent to main app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
 
+//components
+import Layout from '../components/Layout';
+
+//router
+import { useRouter } from 'next/router';
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <Layout>
+          <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
