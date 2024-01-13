6bd1c128a40accc11b1685abfac25665923284b7
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-13 15:37:12 -0500

    [feature] Add name, subtitle, and 'My projects' button to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import ProjectsBtn from '../components/ProjectsBtn';
 const Home = () => {
-  return <div>Home</div>;
+  return (
+      {/* Text */}
+      <div>
+        <div>
+          {/* Name */}
+          <h1>
+            Hugo&nbsp;<span className="text-accent">Arriojas</span>
+          </h1>
+          <p>
+            Software engineer & scrum master with experience in white label
+            e-commerce solutions
+          </p>
+          <div>
+            <ProjectsBtn />
+          </div>
+        </div>
+      </div>
+  );
 };
 
 export default Home;
