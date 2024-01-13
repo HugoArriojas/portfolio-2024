1d78d39a5901b1ad1ce374356dfdefdbb899433d
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-13 16:18:58 -0500

    [style] Add motion to name, subtitle, and 'My Projects' button so they move into viewport on load

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,51 @@
 import ProjectsBtn from '../components/ProjectsBtn';
+
+// framer motion
+import { motion } from 'framer-motion';
+
+//variants
+import { fadeIn } from '../variants';
+
 const Home = () => {
   return (
       {/* Text */}
       <div>
         <div>
           {/* Name */}
-          <h1>
+          <motion.h1
+            variants={fadeIn('down', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Hugo&nbsp;<span className="text-accent">Arriojas</span>
-          </h1>
-          <p>
+          </motion.h1>
+          {/* Subtitle */}
+          <motion.p
+            variants={fadeIn('down', 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-md xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
             Software engineer & scrum master with experience in white label
             e-commerce solutions
+          </motion.p>
+          {/* button */}
           </p>
           <div>
             <ProjectsBtn />
           </div>
+          <motion.div
+            variants={fadeIn('right', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden xl:flex"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
   );
