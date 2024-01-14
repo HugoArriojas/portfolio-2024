5a55e6ad252c9363cbff9bf4d2fbf296b0556141
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-14 10:09:29 -0500

    [feature] Add header logo svg

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,24 @@
+// next image
+import Image from 'next/image';
+
+// next link
+import Link from 'next/link';
 const Header = () => {
-  return <header>Header</header>;
+  return (
+      <header>
+      <div className="container mx-auto">
+          <Link href={'/'}>
+            <Image
+              src={'/logo2.svg'}
+              width={220}
+              height={48}
+              alt="Hugo Arriojas Logo"
+              priority={true}
+            />
+          </Link>
+        </div>
+    </header>
+  );
 };
 
 export default Header;
