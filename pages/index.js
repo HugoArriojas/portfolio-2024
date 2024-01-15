612ada8fea8e9acc64458d41743bdead1373df75
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-15 00:21:56 -0500

    [feature] Add particles container to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+//components
+import ParticlesContainer from '../components/ParticlesContainer';
 import ProjectsBtn from '../components/ProjectsBtn';
 import Headshot from '../components/Headshot';
 
@@ -52,7 +54,7 @@ const Home = () => {
       {/* image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {/* bg image */}
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        <ParticlesContainer />
         {/* headshot */}
         <motion.div
           variants={fadeIn('up', 0.3)}
