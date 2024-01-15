c8827676834ae66da11e8099b32ba9205b83dd53
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-14 22:01:52 -0500

    [feature] Add social links for Github & LinkedIn to be used in header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,28 @@
+// Links
+import Link from 'next/link';
+
+// icons
+import { RiLinkedinBoxFill, RiGithubFill } from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={'https://linkedin.com/in/hugoarriojas'}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300 relative flex items-center group"
+      >
+        <RiLinkedinBoxFill />
+      </Link>
+      <Link
+        href={'https://github.com/HugoArriojas'}
+        target="_blank"
+        className="hover:text-accent transition-all duration-300 relative flex items-center group"
+      >
+        <RiGithubFill />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
