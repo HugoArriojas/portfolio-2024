125028e111df291741bd8014f3a9fe072f99b857
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-14 22:02:31 -0500

    [style] Add tooltips for Github and LinkedIn social links in header

--- components/Socials.js
+++ components/Socials.js
@@ -13,6 +13,16 @@ const Socials = () => {
         className="hover:text-accent transition-all duration-300 relative flex items-center group"
       >
         <RiLinkedinBoxFill />
+        {/* Tooltip */}
+        <div className="absolute top-9 -right-6 hidden group-hover:flex">
+          <div className="bg-white relative flex items-center p-[6px] rounded-[3px]">
+            <p className="text-[12px] leading-none font-semibold capitalize text-primary">
+              LinkedIn
+            </p>
+            {/* triangle */}
+            <div className="border-solid border-b-white border-b-8 border-r-transparent border-l-transparent border-r-[6px] border-l-[6px] absolute -top-2 left-5"></div>
+          </div>
+        </div>
       </Link>
       <Link
         href={'https://github.com/HugoArriojas'}
@@ -20,6 +30,16 @@ const Socials = () => {
         className="hover:text-accent transition-all duration-300 relative flex items-center group"
       >
         <RiGithubFill />
+        {/* Tooltip */}
+        <div className="absolute top-9 -left-4 hidden group-hover:flex">
+          <div className="bg-white relative flex items-center p-[6px] rounded-[3px]">
+            <p className="text-[12px] leading-none font-semibold capitalize text-primary">
+              Github
+            </p>
+            {/* triangle */}
+            <div className="border-solid border-b-white border-b-8 border-r-transparent border-l-transparent border-r-[6px] border-l-[6px] absolute -top-2 left-5"></div>
+          </div>
+        </div>
       </Link>
     </div>
   );
