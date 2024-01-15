aae5fef4c5c43bab7f9e5cfcee5c1f984895cd2f
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-15 00:26:57 -0500

    [task] Import skill icons, add experience to data array

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,15 @@
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaPhp,
+  FaWordpress,
+  FaFigma,
+} from 'react-icons/fa';
+
+import { SiNextdotjs, SiAdobexd, SiAdobephotoshop } from 'react-icons/si';
 
 //  data
 const aboutData = [
@@ -7,12 +19,49 @@ const aboutData = [
       {
         title: 'Web Development',
         icons: [
+          <FaHtml5 />,
+          <FaCss3 />,
+          <FaJs />,
+          <FaReact />,
+          <FaPhp />,
+          <SiNextdotjs />,
+          <FaWordpress />,
         ],
       },
       {
         title: 'UI/UX Design',
-        icons: [
-      ],
+        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+      },
+    ],
+  },
+  {
+    title: 'experience',
+    info: [
+      {
+        title: 'Software Engineer & Scrum Master - Mercatus Technologies Inc.',
+        stage: '2022 - Present',
+      },
+      {
+        title: 'Freelance Web Developer',
+        stage: '2021 - 2022',
+      },
+      {
+        title: 'Accounting Administrator - Carson Auto Group',
+        stage: '2018 - 2021',
+      },
+    ],
+  },
+  {
+    title: 'credentials',
+    info: [
+      {
+        title: 'Web Development Immersive Certificate - Juno College',
+        stage: '2021',
+      },
+      {
+        title:
+          'Honours Bachelor of Science in Marine Biology & Statistics - Dalhousie University',
+        stage: '2013 - 2017',
       },
     ],
   },
