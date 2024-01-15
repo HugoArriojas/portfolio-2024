83f7105afc829797402eaac09a30b22dccfcc976
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-14 22:08:52 -0500

    [feature] Add socials to header, update logo

--- components/Header.js
+++ components/Header.js
@@ -3,20 +3,24 @@ import Image from 'next/image';
 
 // next link
 import Link from 'next/link';
+
+// components
+import Socials from '../components/Socials';
+
 const Header = () => {
   return (
       <header>
       <div className="container mx-auto">
           <Link href={'/'}>
             <Image
-              src={'/logo2.svg'}
+              src={'/hugo-logo.svg'}
               width={220}
               height={48}
               alt="Hugo Arriojas Logo"
               priority={true}
             />
           </Link>
-        </div>
+          <Socials />
     </header>
   );
 };
