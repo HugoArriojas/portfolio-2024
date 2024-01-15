bef4402c0fb19aaf737aac1496bc383b473e0801
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-15 00:38:00 -0500

    [bugfix] Fix lint on Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
 //fonts
-import {Sora} from '@next/font/google'
+import { Sora } from '@next/font/google'
 
 // font settings
 const sora = Sora({
   subsets: ['latin'],
   variable: '--font-sora',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
 })
 
 //components
@@ -13,13 +13,19 @@ import Nav from '../components/Nav'
 import Header from '../components/Header'
 import TopLeftImg from '../components/TopLeftImg'
 
-const Layout = ({children}) => {
-  return <div>
-          <TopLeftImg/>
-          <Nav/>
-          <Header/>
-          {children}
-        </div>;
-};
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={
+        'page bg-site text-white bg-cover bg-no-repeat ${sora,variable} font-sora relative'
+      }
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  )
+}
 
-export default Layout;
+export default Layout
