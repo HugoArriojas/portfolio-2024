e456f0e76cf270579ca139b246de6d551a8a251b
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-15 00:16:31 -0500

    [style] Add styling to header to spread out social icons from header title

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,9 @@ import Socials from '../components/Socials';
 
 const Header = () => {
   return (
-      <header>
+    <header className="absolute z-50 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           <Link href={'/'}>
             <Image
               src={'/hugo-logo.svg'}
@@ -21,6 +22,8 @@ const Header = () => {
             />
           </Link>
           <Socials />
+        </div>
+      </div>
     </header>
   );
 };
