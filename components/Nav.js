1e4e9b34f49cbf8ed7baa5cccff8fe9d5e923415
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-16 20:13:47 -0500

    [feature] Add rings loader to nav icons to indicate when navication has been selected

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,9 @@
 // icons
-import {
-  HiHome,
-  HiUser,
-  HiViewColumns,
-  HiEnvelope,
-} from 'react-icons/hi2';
+import { HiHome, HiUser, HiViewColumns, HiEnvelope } from 'react-icons/hi2';
 
 import { RiArticleFill } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { Rings } from 'react-loader-spinner';
 
 // nav data
 export const navData = [
@@ -33,23 +30,24 @@ import { useRouter } from 'next/router';
 
 const Nav = () => {
   const router = useRouter();
-  const pathname = router.pathname;
+  const currentPathname = router.pathname;
+  const [directedPathname, setPathname] = useState(currentPathname);
+
   return (
     <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
-      {/* Inner */}
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((link, index) => {
           return (
             <Link
               className={`${
-                link.path === pathname && 'text-accent'
+                link.path === currentPathname && 'text-accent'
               } relative flex items-center group hover:text-accent transition-all duration-300`}
               href={link.path}
               key={index}
-              target={link.target}
+              onClick={() => setPathname(link.path)}
             >
-              {/* Tooltip */}
-              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex ">
+              {/* tooltip */}
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
                 <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
                   <div className="text-[12px] leading-none font-semibold capitalize">
                     {link.name}
@@ -58,8 +56,21 @@ const Nav = () => {
                   <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
                 </div>
               </div>
-              {/* Icon */}
-              {link.icon}
+              {/* icon */}
+              <div>
+                {directedPathname === link.path &&
+                currentPathname !== link.path ? (
+                  <Rings
+                    height="20"
+                    width="20"
+                    color="#fe6192"
+                    ariaLabel="rings-loading"
+                    wrapperClass=""
+                  />
+                ) : (
+                  link.icon
+                )}
+              </div>
             </Link>
           );
         })}
