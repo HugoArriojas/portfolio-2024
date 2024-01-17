2c8ea2d6fb6044184380b7ed8caf58fb01f5cd94
HugoArriojas <hugo.arriojas@hotmail.com>
2024-01-17 18:56:13 -0500

    [feature] Add basic layout for work page along with styling background images

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,41 @@
+// components
+import WorkSlider from '../../components/WorkSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div >
+      <div className="h-full container mx-auto flex flex-col align-middle">
+        <div className="flex flex-col">
+          {/* text */}
+          <div className="text-center flex flex-col  xl:mb-0 ">
+            <h2
+            >
+              My work
+            </h2>
+            <p
+            >
+              Having worked at Mercatus, I have a great deal of experience
+              creating accessible & scalable white-label e-commerce products for
+              a variety of clients.
+              <br />
+              As I&apos;m in the process of obtaining permission to cite
+              examples, here are some of my projects from bootcamp:
+            </p>
+          </div>
+
+          {/* slider */}
+          <div
+          >
+            <WorkSlider />
+          </div>
+        </div>
+      </div>
+      <Circles />
+      <Bulb />
+    </div>
+  );
 };
 
 export default Work;
