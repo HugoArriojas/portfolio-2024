3e38245573457843cfb581e08fef98b0fa70a70b
HugoArriojas <hugo.arriojas@hotmail.com>
2024-02-01 22:38:11 -0500

    [feature] Move work constants from work index to separate file

---
+++ pages/work/constants.js
@@ -0,0 +1,64 @@
+export const workObjects = {
+  mercatus: {
+    title: 'Mercatus Technologies Inc.',
+    path: '/mercatus.png',
+    detailsLink: '/work/mercatus',
+    liveLink: 'https://hugo-portfolio-2022.netlify.app/',
+    blurb:
+      'Previous portfolio page before redesign, note the similar colour scheme to my current portfolio!',
+    github: 'https://github.com/HugoArriojas/portfolio-2024',
+    technologies: 'React, TypeScript, Apollo, AngularJS, Next.JS, Docker',
+  },
+  previousPortfolio: {
+    title: 'Previous Portfolio',
+    path: '/previous-portfolio.png',
+    detailsLink: '/work/previous-portfolio',
+    liveLink: 'https://hugo-portfolio-2022.netlify.app/',
+    blurb:
+      'Previous portfolio page before redesign, note the similar colour scheme to my current portfolio!',
+    github: 'https://github.com/HugoArriojas/portfolio-2024',
+    technologies: 'HTML, CSS, JavaScript, jQuery, Sass, Figma',
+  },
+  reactiveRetail: {
+    title: 'Reactive Retail',
+    path: '/reactive-retail.png',
+    detailsLink: '/work/reactive-retail',
+    liveLink: 'https://reactiveretail.netlify.app/',
+    blurb:
+      'Mock eCommerce site built in React using the FakeStore API and Firebase integration; currently working on a backend',
+    github: 'https://github.com/HugoArriojas/ReactiveRetail',
+    technologies: 'React, Sass, Figma, Photoshop',
+  },
+  gifMeTheWeather: {
+    title: 'GIF Me the Weather',
+    path: '/gif-weather.png',
+    detailsLink: '/work/gif-weather',
+    liveLink: 'https://gifmetheweather.netlify.app/',
+    blurb:
+      'Weather app created in pair programming exercise using various APIs to receive a weekly forecast and activity suggestions',
+    github: 'https://github.com/hugElla/gifMeTheWeather',
+    technologies: 'HTML, CSS, JavaScript, jQuery, Sass, Figma',
+  },
+  notSoSocial: {
+    title: 'Not So Social Planner',
+    path: '/not-social.png',
+    detailsLink: '/work/not-social',
+    liveLink: 'https://notsosocialplanner.netlify.app/',
+    blurb:
+      'Agency-style project for a social planner where users can slot in either activities or actively streaming shows',
+    github: 'https://github.com/Hugo-Liz-Linda-Ryan/NotSoSocialEventPlanner',
+    technologies: 'React, Sass, Figma',
+  },
+  delicious: {
+    title: 'Delicious',
+    path: '/delicious.png',
+    detailsLink: '/work/delicious',
+    liveLink: 'https://deliciouspsd.netlify.app/',
+    blurb:
+      'Multi-page conversion of PSD design in a mock client request exercice. My first web development project ever!',
+    github: 'https://github.com/HugoArriojas/deliciousPSDConversion',
+    technologies: 'HTML, CSS, JavaScript, jQuery, Sass, Figma',
+  },
+};
+
+export const worksArray = Object.keys(workObjects);
--- pages/work/index.js
+++ pages/work/index.js
@@ -6,74 +6,13 @@ import Link from 'next/link';
 // framer motion
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
-import { useRef, useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 // icons
 import { BsArrowRight } from 'react-icons/bs';
 // next image
 import Image from 'next/image';
 
-export const workLinks = [
-  {
-    title: 'Mercatus Technologies Inc.',
-    path: '/mercatus.png',
-    detailsLink: '/work/mercatus',
-    liveLink: 'https://hugo-portfolio-2022.netlify.app/',
-    blurb:
-      'Previous portfolio page before redesign, note the similar colour scheme to my current portfolio!',
-    github: 'https://github.com/HugoArriojas/portfolio-2024',
-    technologies: 'React, TypeScript, Apollo, AngularJS, Next.JS, Docker',
-  },
-  {
-    title: 'Previous Portfolio',
-    path: '/previous-portfolio.png',
-    detailsLink: '/work/previous-portfolio',
-    liveLink: 'https://hugo-portfolio-2022.netlify.app/',
-    blurb:
-      'Previous portfolio page before redesign, note the similar colour scheme to my current portfolio!',
-    github: 'https://github.com/HugoArriojas/portfolio-2024',
-    technologies: 'HTML, CSS, JavaScript, jQuery, Sass, Figma',
-  },
-  {
-    title: 'Reactive Retail',
-    path: '/reactive-retail.png',
-    detailsLink: '/work/reactive-retail',
-    liveLink: 'https://reactiveretail.netlify.app/',
-    blurb:
-      'Mock eCommerce site built in React using the FakeStore API and Firebase integration; currently working on a backend',
-    github: 'https://github.com/HugoArriojas/ReactiveRetail',
-    technologies: 'React, Sass, Figma, Photoshop',
-  },
-  {
-    title: 'GIF Me the Weather',
-    path: '/gif-weather.png',
-    detailsLink: '/work/gif-weather',
-    liveLink: 'https://gifmetheweather.netlify.app/',
-    blurb:
-      'Weather app created in pair programming exercise using various APIs to receive a weekly forecast and activity suggestions',
-    github: 'https://github.com/hugElla/gifMeTheWeather',
-    technologies: 'HTML, CSS, JavaScript, jQuery, Sass, Figma',
-  },
-  {
-    title: 'Not So Social Planner',
-    path: '/not-social.png',
-    detailsLink: '/work/not-social',
-    liveLink: 'https://notsosocialplanner.netlify.app/',
-    blurb:
-      'Agency-style project for a social planner where users can slot in either activities or actively streaming shows',
-    github: 'https://github.com/Hugo-Liz-Linda-Ryan/NotSoSocialEventPlanner',
-    technologies: 'React, Sass, Figma',
-  },
-  {
-    title: 'Delicious',
-    path: '/delicious.png',
-    detailsLink: '/work/delicious',
-    liveLink: 'https://deliciouspsd.netlify.app/',
-    blurb:
-      'Multi-page conversion of PSD design in a mock client request exercice. My first web development project ever!',
-    github: 'https://github.com/HugoArriojas/deliciousPSDConversion',
-    technologies: 'HTML, CSS, JavaScript, jQuery, Sass, Figma',
-  },
-];
+import { workObjects, worksArray } from './constants';
 
 const Work = () => {
   useEffect(() => {
