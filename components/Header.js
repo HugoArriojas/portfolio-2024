8ebc98f3806255995066070d08d34058e2fd1abb
HugoArriojas <hugo.arriojas@hotmail.com>
2024-02-03 15:55:47 -0500

    [bugfix] Increase z-index of header and fix linting errors

--- components/Header.js
+++ components/Header.js
@@ -9,15 +9,15 @@ import Socials from '../components/Socials';
 
 const Header = () => {
   return (
-    <header className="absolute z-50 flex h-[85px] w-full items-center xl:px-0">
-      <div className="container mx-auto">
-        <div className="flex flex-row items-center justify-between px-2 pb-5 pt-8 md:px-0">
+    <header className='absolute z-[51] flex h-[85px] w-full items-center xl:px-0'>
+      <div className='container mx-auto'>
+        <div className='flex flex-row items-center justify-between px-2 pb-5 pt-8 md:px-0'>
           <Link href={'/'}>
             <Image
               src={'/hugo-logo.svg'}
               width={220}
               height={48}
-              alt="Hugo Arriojas Logo"
+              alt='Hugo Arriojas Logo'
               priority={true}
             />
           </Link>
