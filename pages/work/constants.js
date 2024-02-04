6ec9115e335e7128d030c56074771f1027f19e23
HugoArriojas <hugo.arriojas@hotmail.com>
2024-02-03 21:17:36 -0500

    [bugfix] Add alt text to links for work pages for accessibility

--- pages/work/constants.js
+++ pages/work/constants.js
@@ -4,6 +4,7 @@ export const workObjects = {
     path: '/mercatus.png',
     detailsLink: '/work/mercatus',
     liveLink: 'https://hugo-portfolio-2022.netlify.app/',
+    alt: 'Read more about my work experience at Mercatus',
     blurb:
       'Previous portfolio page before redesign, note the similar colour scheme to my current portfolio!',
     github: 'https://github.com/HugoArriojas/portfolio-2024',
@@ -14,6 +15,7 @@ export const workObjects = {
     path: '/previous-portfolio.png',
     detailsLink: '/work/previous-portfolio',
     liveLink: 'https://hugo-portfolio-2022.netlify.app/',
+    alt: 'Read more about my previous portfolio',
     blurb:
       'Previous portfolio page before redesign, note the similar colour scheme to my current portfolio!',
     github: 'https://github.com/HugoArriojas/portfolio-2024',
@@ -24,6 +26,7 @@ export const workObjects = {
     path: '/reactive-retail.png',
     detailsLink: '/work/reactive-retail',
     liveLink: 'https://reactiveretail.netlify.app/',
+    alt: 'Read more about my reactive retail bootcamp project',
     blurb:
       'Mock eCommerce site built in React using the FakeStore API and Firebase integration; currently working on a backend',
     github: 'https://github.com/HugoArriojas/ReactiveRetail',
@@ -34,6 +37,7 @@ export const workObjects = {
     path: '/gif-weather.png',
     detailsLink: '/work/gif-weather',
     liveLink: 'https://gifmetheweather.netlify.app/',
+    alt: 'Read more about my weather app built in bootcamp',
     blurb:
       'Weather app created in pair programming exercise using various APIs to receive a weekly forecast and activity suggestions',
     github: 'https://github.com/hugElla/gifMeTheWeather',
@@ -44,6 +48,7 @@ export const workObjects = {
     path: '/not-social.png',
     detailsLink: '/work/not-social',
     liveLink: 'https://notsosocialplanner.netlify.app/',
+    alt: 'Read more about my agency-style project built in bootcamp',
     blurb:
       'Agency-style project for a social planner where users can slot in either activities or actively streaming shows',
     github: 'https://github.com/Hugo-Liz-Linda-Ryan/NotSoSocialEventPlanner',
@@ -54,11 +59,10 @@ export const workObjects = {
     path: '/delicious.png',
     detailsLink: '/work/delicious',
     liveLink: 'https://deliciouspsd.netlify.app/',
+    alt: 'Read more about my first web dev project, a PSD conversion',
     blurb:
       'Multi-page conversion of PSD design in a mock client request exercice. My first web development project ever!',
     github: 'https://github.com/HugoArriojas/deliciousPSDConversion',
     technologies: 'HTML, CSS, JavaScript, jQuery, Sass, Figma',
   },
 };
-
-export const worksArray = Object.keys(workObjects);
