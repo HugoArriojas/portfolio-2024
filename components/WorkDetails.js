1a30eb32de85d5a98a31fd0f8e5e620024520437
HugoArriojas <hugo.arriojas@hotmail.com>
2024-02-03 21:16:49 -0500

    [feature] Create repeatable WorkDetails component for each project

--- components/WorkDetails.js
+++ components/WorkDetails.js
@@ -1,5 +1,35 @@
-function WorkDetails() {
-  return <div>WorkDetails will go here</div>;
-}
+// framer motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+// icons
+import { BsArrowRight } from 'react-icons/bs';
+// next image
+import Image from 'next/image';
+import Link from 'next/link';
+
+const WorkDetails = (props) => {
+  return (
+    <div className='flex flex-col text-center xl:mb-0  xl:w-[30vw]'>
+      <motion.h2
+        variants={fadeIn('up', 0.2)}
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className='h2'
+      >
+        {props.details.title}
+      </motion.h2>
+      <motion.div
+        variants={fadeIn('down', 0)}
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        className='xl:max-w-[65%]'
+      >
+        {props.details.blurb}
+      </motion.div>
+    </div>
+  );
+};
 
 export default WorkDetails;
