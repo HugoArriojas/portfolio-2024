6d2d455ef50deb34cbb1c10ccd38f7e0014955b0
HugoArriojas <hugo.arriojas@hotmail.com>
2024-02-06 22:32:24 -0500

    [feature] Add first draft of Mercatus constants

--- pages/work/constants.js
+++ pages/work/constants.js
@@ -5,10 +5,16 @@ export const workObjects = {
     detailsLink: '/work/mercatus',
     liveLink: 'https://hugo-portfolio-2022.netlify.app/',
     alt: 'Read more about my work experience at Mercatus',
-    blurb:
-      'Previous portfolio page before redesign, note the similar colour scheme to my current portfolio!',
+    blurb: 'White label e-commerce solutions',
     github: 'https://github.com/HugoArriojas/portfolio-2024',
-    technologies: 'React, TypeScript, Apollo, AngularJS, Next.JS, Docker',
+    technologies:
+      'React, TypeScript, Apollo GraphQL, PHP, AngularJS, NextJS, Docker, Drupal, Bamboo, JIRA',
+    about:
+      'As a software engineer and Scrum Master at Mercatus Technologies, I was responsible for both developing and debugging our accessible and functional white label components for our retailers. Working mostly in front-end technologies, I led a cross-functional scrum team of 9 people through a variety of scrum ceremonies, achieving over 90% completion of all scheduled tickets in our sprints due to my sprint planning and project management skills.',
+    challenges:
+      'One major challenge I faced at Mercatus was having learning AngularJS and PHP on the job while working on a mature codebase. Although I was familiar with other frameworks such as React, at times, I was the sole contributor to the v3 implementation of the white-label product, meaning that I had to learn these languages in a short amount of time. My goal was to not only learn these new languages, but further expand my skill set by embracing opportunity where possible. As such, for my second task I accepted the challenge to implement the carousel that is now seen on the homepage of all Mercatus sites. I then worked on some major reworks of the site including (but not limited to) an update to the categorization hierarchy on our shop page, and WCAG updates to our production codebase. As a result, there was an increase of 26% in user interactions regarding the product hierarchy, and a marked 90% adoption of all of the components that I was tasked with building.',
+    favourite:
+      'My favourite part about working at Mercatus was creating something accessible and useful that facilitated real revenue generation for our clients and their member businesses. Having worked with a variety of US retailers, the product I assisted in building gave smaller grocery retailers a chance in an increasingly electronic space, especially when faced with the monopolization of larger companies. Another favourite part of mine was taking on the role of Scrum Master while also remaining as an individual contributor. As the Scrum Master with the shortest amount of tenure at the company, I felt honoured to be chosen and I am incredibly grateful for the opportunity that I was given to both develop my skills as an individual contributor, and to be able to delve into the world of project management and Agile development from such a unique perspective.',
   },
   previousPortfolio: {
     title: 'Previous Portfolio',
