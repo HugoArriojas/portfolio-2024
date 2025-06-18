1cf0659bad02449a9c7ef63aa510d4a626617463
Hugo Arriojas <hugo.arriojas@outlook.com>
2025-06-17 21:04:02 -0400

    Feature: Update Payload config to use Vercel Blob storage

--- payload.config.ts
+++ payload.config.ts
@@ -1,39 +1,76 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb';
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud';
-import { lexicalEditor } from '@payloadcms/richtext-lexical';
+import sharp from 'sharp'; // sharp-import
 import path from 'path';
 import { buildConfig } from 'payload';
 import { fileURLToPath } from 'url';
-import sharp from 'sharp';
-
-import { Users } from './collections/Users';
 import { Media } from './collections/Media';
-import { Work } from './collections/Work';
+import { Users } from './collections/Users';
+import { defaultLexical } from '@/fields/defaultLexical';
+import { getServerSideURL } from './utilities/getURL';
+import { SiteSettings } from './globals/Settings';
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
-    user: Users.slug,
     importMap: {
       baseDir: path.resolve(dirname),
     },
+    user: Users.slug,
+    components: {
+      graphics: {
+        Icon: '@/components/ui/icon',
+        Logo: '@/components/ui/logo',
+      },
+    },
+    livePreview: {
+      breakpoints: [
+        {
+          label: 'Mobile',
+          name: 'mobile',
+          width: 375,
+          height: 667,
+        },
+        {
+          label: 'Tablet',
+          name: 'tablet',
+          width: 768,
+          height: 1024,
+        },
+        {
+          label: 'Desktop',
+          name: 'desktop',
+          width: 1440,
+          height: 900,
+        },
+      ],
+    },
   },
-  // globals: [Homepage],
-  collections: [Users, Media, Work],
-  editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
+  // This config helps us configure global or default features that the other editors can inherit
+  editor: defaultLexical,
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
   }),
-  sharp,
+  collections: [Media, Users],
+  cors: [getServerSideURL()].filter(Boolean),
+  globals: [SiteSettings],
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: process.env.VERCEL_BLOB_STORAGE_ENABLED === 'true',
+      clientUploads: process.env.VERCEL_BLOB_BYPASS_UPLOAD_LIMIT === 'true',
+      collections: {
+        [Media.slug]: true,
+        [exports]: true,
+      },
+      token: process.env.VERCEL_BLOB_READ_WRITE_TOKEN || '',
+    }),
   ],
+  secret: process.env.PAYLOAD_SECRET || '',
+  sharp,
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
 });
